d118464ec53bd1ed7b122eead98f166bcfeae47a
elducati <geomondi09@gmail.com>
2020-07-15 14:58:04 +0300

    added comments

--- quote.js
+++ quote.js
@@ -1,4 +1,4 @@
-
+//function displays quotes per category
 function randomQuotePerCategory(
   quote,
   randomNietzsche,
@@ -12,31 +12,36 @@ function randomQuotePerCategory(
     const nietzscheQuoteBegin = quote.nietzscheQuote.begin;
     const nietzscheQuoteMiddle = quote.nietzscheQuote.middle;
     const nietzscheQuoteEnd = quote.nietzscheQuote.end;
+    //randomly displays a quote when category is selected
     document.getElementById("quote-display").innerHTML = `"${nietzscheQuoteBegin[randomNietzsche]} ${nietzscheQuoteMiddle[randomNietzsche]} ${nietzscheQuoteEnd[randomNietzsche]}"`;
     document.getElementById("quote-heading").textContent = "Nietzsche Quotes";
+    //call function to display quotes depending on number selected
     numNietzscheQuote(nietzscheQuoteBegin, nietzscheQuoteMiddle, nietzscheQuoteEnd);
   } else if (category == "Morning") {
     const morningQuoteBegin = quote.morningQuote.begin;
     const morningQuoteMiddle = quote.morningQuote.middle;
     const morningQuoteEnd = quote.morningQuote.end;
+    //randomly displays a quote when category is selected
     document.getElementById(
       "quote-display"
     ).innerHTML = `"${morningQuoteBegin[randomMorning]} ${morningQuoteMiddle[randomMorning]} ${morningQuoteEnd[randomMorning]}"`;
     document.getElementById("quote-heading").textContent = "Morning Quotes";
+    //call function to display quotes depending on number selected
     numMorningQuote(morningQuoteBegin, morningQuoteMiddle, morningQuoteEnd);
   } else if (category == "Popular") {
     const popQuoteBegin = quote.popQuote.begin;
     const popQuoteMiddle = quote.popQuote.middle;
     const popQuoteEnd = quote.popQuote.end;
-
+    //randomly displays a quote when category is selected
     document.getElementById("quote-heading").textContent = "Popular Quotes";
     document.getElementById(
       "quote-display"
     ).innerHTML = `"${popQuoteBegin[randomPopular]} ${popQuoteMiddle[randomPopular]} ${popQuoteEnd[randomPopular]}"`;
+    //call function to display quotes depending on number selected
     numPopularQuote(popQuoteBegin, popQuoteMiddle, popQuoteEnd);
 
   }
-
+//functions to display quotes depending on the number selected and per category
   function numPopularQuote(popQuoteBegin, popQuoteMiddle, popQuoteEnd) {
 
     for (let i = 0; i <= numOfQuotes - 1; i++) {
@@ -63,21 +68,15 @@ function randomQuotePerCategory(
     }
   }
 }
-function newFunctionRandom(morning, popular, nietzsche) {
-  let randomPopular = Math.floor(morning);
-  let randomMorning = Math.floor(popular);
-  let randomNietzsche = Math.floor(nietzsche);
-  return { randomNietzsche, randomMorning, randomPopular };
-}
-
+ //use math.random to get a random number between 0 and 1 then multiply by the length of each section of the object
 function newFunctionQuote(quote) {
   const morningQuoteBeginLength = quote.morningQuote.begin.length;
   const morningQuoteMiddleLength = quote.morningQuote.middle.length;
   const morningQuoteEndLength = quote.morningQuote.end.length;
+  
   const mathDotRandomMorning =
     Math.random() *
-    (morningQuoteBeginLength, morningQuoteMiddleLength, morningQuoteEndLength);
-  //use math.random to get a random number between 0 and 1 then multiply by the length of each section of the object
+    (morningQuoteBeginLength, morningQuoteMiddleLength, morningQuoteEndLength); 
   let morning = mathDotRandomMorning;
   const popQuoteBeginLength = quote.popQuote.begin.length;
   const popQuoteMiddleLength = quote.popQuote.middle.length;
@@ -95,5 +94,16 @@ function newFunctionQuote(quote) {
       nietzscheQuoteMiddleLength,
       nietzscheQuoteEndLength);
   let nietzsche = mathDotRandomNietzsche;
+  //return the random number generated for each section of the object 'quote'
   return { morning, popular, nietzsche };
 }
+
+//round off the result to the nearest integer value that is less than or equal to the random number
+function newFunctionRandom(morning, popular, nietzsche) {
+  let randomPopular = Math.floor(morning);
+  let randomMorning = Math.floor(popular);
+  let randomNietzsche = Math.floor(nietzsche);
+  return { randomNietzsche, randomMorning, randomPopular };
+}
+
+
